88c7ed4407bd46f2ef46db47230f2ddb2cdd6bb8
YellowSwerve <57231393+YellowSwerve@users.noreply.github.com>
2021-01-27 21:48:13 -0800

    Merge branch 'main' into ricky_ui_move_in

---
+++ Assets/CombatShit/Scripts/weaponBehaviour.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponBehaviour : MonoBehaviour
+{
+    public int index;
+    public GameObject[] weapons;
+    public GameObject weaponAnchor;
+    public GameObject currentWeapon;
+
+    private int totalWeapons = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalWeapons = weaponAnchor.transform.childCount;
+        weapons = new GameObject[totalWeapons];
+        for(int i = 0; i < totalWeapons; i++)
+        {
+            weapons[i] = weaponAnchor.transform.GetChild(i).gameObject;
+            weapons[i].SetActive(false);
+        }
+        weapons[0].SetActive(true);
+        currentWeapon = weapons[0];
+        index = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            if(index < totalWeapons - 1)
+            {
+                weapons[index].SetActive(false);
+                index++;
+                weapons[index].SetActive(true);
+            }
+            else
+            {
+                weapons[index].SetActive(false);
+                index = 0;
+                weapons[index].SetActive(true);
+            }
+            currentWeapon = weapons[index];
+        }
+        else if (Input.mouseScrollDelta.y < 0)
+        {
+            if (index > 0)
+            {
+                weapons[index].SetActive(false);
+                index--;
+                weapons[index].SetActive(true);
+            }
+            else
+            {
+                weapons[index].SetActive(false);
+                index = totalWeapons - 1;
+                weapons[index].SetActive(true);
+            }
+            currentWeapon = weapons[index];
+        }
+    }
+}
---
+++ Assets/The Lucio Zone/UI Scripts/InventoryTracker.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class InventoryTracker : MonoBehaviour
+{
+    #region Public Variables
+    [Header("In Scene References")]
+    [Tooltip("Takes in a Sprite and a name associated with that Sprite. The name should be the same as key used in inventoryDict. Place in order of appearance in inventory menu.")]
+    public List<IngredientImage> ingredientPictures = new List<IngredientImage>();
+    
+    #endregion
+    //dictionary of objects being stored
+    //key is name of ingredient
+    //value is number of ingredient in inventory
+    private Dictionary<string, int> inventoryDict = new Dictionary<string, int>();
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] search = GameObject.FindGameObjectsWithTag("InventoryTracker");
+        if(search.Length > 1){
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //returns the amount of an ingredient
+    //returns -1 if the ingredient has not been discovered
+    public int getAmount(string ingredient){
+        if(inventoryDict.ContainsKey(ingredient)){
+            return inventoryDict[ingredient];
+        }
+        else{
+            return -1;
+        }
+    }
+
+    //subtracts modifier from amount in inventory linked to key
+    //returns true if subtraction successful
+    //returns false if there aren't enough ingredients or ingredient doesn't exist
+    public bool subtract(string ingredient, int modifier){
+        //check if ingredient has been found
+        if(inventoryDict.ContainsKey(ingredient)){
+            int temp = inventoryDict[ingredient];
+            temp -= modifier;
+            if(temp < 0){
+                return false;
+            }
+            //can't subtract
+            else{
+                inventoryDict[ingredient] = temp;
+                return true;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+
+    //adds modifier to amount in inventory linked to key
+    //returns true if addition succesful
+    //returns false if addition couldn't be done
+    public bool add(string ingredient, int modifier){
+        //check if ingredient has been found
+        if(inventoryDict.ContainsKey(ingredient)){
+            int temp = inventoryDict[ingredient];
+            temp += modifier;
+            
+            //if addition results in a negative return false
+            if(temp < 0){
+                return false;
+            }
+            //else do addition
+            else{
+                inventoryDict[ingredient] = temp;
+                return true;
+            }
+        }
+        //adds ingredient to inventoryDict, discovering ingredient
+        else{
+            inventoryDict[ingredient] = modifier;
+            return false;
+        }
+    }
+
+    //Adds 1 to the amount of the ingredient indicated by key
+    public void addOne(string ingredient){
+        //check if ingredient has been found
+        if(inventoryDict.ContainsKey(ingredient)){
+            int temp = inventoryDict[ingredient];
+            temp += 1;
+            if(temp < 0){
+
+            }
+            else{
+                inventoryDict[ingredient] = temp;
+            }
+        }
+        else{
+            inventoryDict[ingredient] = 1;
+        }
+
+        // if(ingredient == "Carrot"){
+        //     SceneManager.LoadScene(2);
+        // }
+    }
+
+    //returns true if dictionary already has given key
+    //returns false if key not found
+    public bool discovered(string ingredient){
+        return inventoryDict.ContainsKey(ingredient);
+    }
+
+    public Sprite getPic(string ingredient){
+        foreach(IngredientImage image in ingredientPictures){
+            if(image.name == ingredient){
+                return image.picture;
+            }
+        }
+        return null;
+    }
+    
+    public void GoHome(){
+        SceneManager.LoadScene(0);
+    }
+}
+
+[System.Serializable]
+public class IngredientImage{
+    public Sprite picture;
+    public string name;
+}
+
