5994f543b9f378e818661ba358fed7ab253c0f5c
Ryan Heath <rheath@ucsc.edu>
2021-02-09 04:35:47 -0800

    shotgun

--- Assets/CombatShit/Scripts/WeaponHandlers/ShotgunHandler.cs
+++ Assets/CombatShit/Scripts/WeaponHandlers/ShotgunHandler.cs
@@ -6,42 +6,55 @@ using CodeMonkey.Utils;
 
 public class ShotgunHandler : MonoBehaviour
 {
-    public LineRenderer tracer;
+    public List<LineRenderer> tracers = new List<LineRenderer>(5);
     [SerializeField] private Material weaponTracerMaterial;
-    [SerializeField] private Sprite shootFlashSprite;
+    [SerializeField] public SpriteRenderer muzzleFlash;
+    public Action<LineRenderer> turnOff;
+
     public void RayShoot(Vector3 EndPoint, Vector3 ShootDir)
     {
-        RaycastHit2D raycastHit2D = Physics2D.Raycast(EndPoint, ShootDir);
-        CreateWeaponTracer(EndPoint, EndPoint, ShootDir);
-        CreateShootFlash(EndPoint);
-        if (raycastHit2D.collider != null)
-        {
-            EnemyBehaviour target = raycastHit2D.collider.GetComponent<EnemyBehaviour>();
-            if (target != null)
+        foreach (LineRenderer tracer in tracers) {
+            float angle = UnityEngine.Random.Range(-5f, 5f);
+            Vector3 newShootDir = (Quaternion.Euler(0f, 0f, angle) * ShootDir);
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(EndPoint, newShootDir);
+            CreateWeaponTracer(EndPoint, EndPoint, newShootDir, tracer);
+            CreateShootFlash();
+            if (raycastHit2D.collider != null)
             {
-                target.TakeDamage(5);
+                EnemyBehaviour target = raycastHit2D.collider.GetComponent<EnemyBehaviour>();
+                if (target != null)
+                {
+                    target.TakeDamage(5);
+                }
             }
         }
     }
 
-    public void CreateWeaponTracer(Vector3 fromPosition, Vector3 targetPosition, Vector3 targetNormal)
+    public void CreateWeaponTracer(Vector3 fromPosition, Vector3 targetPosition, Vector3 targetNormal, LineRenderer tracer)
     {
         targetNormal = targetNormal * Camera.main.orthographicSize * 2.1f;
         tracer.enabled = true;
         tracer.SetPosition(0, fromPosition);
         tracer.SetPosition(1, targetPosition + targetNormal);
-        FunctionTimer.Create(turnoffTracer, .05f);
-    }
+        StartCoroutine(KillTrace(0.08f, tracer));
 
-    public void turnoffTracer()
+    }
+    IEnumerator KillTrace(float time, LineRenderer tracer)
     {
+        yield return new WaitForSeconds(time);
         tracer.enabled = false;
     }
 
-    private void CreateShootFlash(Vector3 spawnPosition)
+    private void CreateShootFlash()
     {
-        World_Sprite worldSprite = World_Sprite.Create(spawnPosition, Vector3.zero, shootFlashSprite, Color.white, 10000);
-        FunctionTimer.Create(worldSprite.DestroySelf, .05f);
+        muzzleFlash.enabled = true;
+        StartCoroutine(killFlash(0.03f, muzzleFlash));
+    }
+
+    IEnumerator killFlash(float time, SpriteRenderer muzzleFlash)
+    {
+        yield return new WaitForSeconds(time);
+        muzzleFlash.enabled = false;
     }
 
     public static void ShakeCamera(float intensity, float timer)
