531c8267a52169bd98c991ad58076f59d4cde3e1
Nikolas Sanchez <yoshiniko1223@gmail.com>
2021-04-28 03:49:32 -0700

    Added tutorial messages
    how do do what do to how do to do wh

---
+++ Assets/Minigames/BookPages.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookPages : MonoBehaviour{
+    [Header("This is where your pages go, expand as necessary")]
+    public GameObject[] PagesArray;
+    private int PageIndex = 0;
+
+
+    public void NextPage(){
+        if(PageIndex + 1 < PagesArray.Length){
+            PagesArray[PageIndex].SetActive(false);
+            PageIndex += 1;
+            PagesArray[PageIndex].SetActive(true);
+        }
+    }
+
+    public void PreviousPage(){
+        if(PageIndex - 1 >= 0){
+            PagesArray[PageIndex].SetActive(false);
+            PageIndex -= 1;
+            PagesArray[PageIndex].SetActive(true);
+        }
+    }
+
+
+
+}
