556b440ef3d99ac6973a85a7d6a89af7ea16e907
Lucio Espinoza <lufespin@ucsc.edu>
2021-05-09 21:02:19 -0700

    Revert "Revert "Merge with main""
    This reverts commit 8c5f7cef09fbfb65f3dd0e21721847c402887fef.

--- Assets/UI/UI Scripts/InventoryTracker.cs
+++ Assets/UI/UI Scripts/InventoryTracker.cs
@@ -27,6 +27,8 @@ public class InventoryTracker : MonoBehaviour
     public float flavor;
     public float volume = 0f;
 
+    public FoodStats CoffeeStats;
+
     #endregion
 
     // Start is called before the first frame update
@@ -37,19 +39,22 @@ public class InventoryTracker : MonoBehaviour
             dialogues.Add(JsonUtility.FromJson<Dialogue>(asset.text));
         }
     }
-    void Start()
-    {
+    void Start(){
         //destroys self if it is a duplicate
         GameObject[] search = GameObject.FindGameObjectsWithTag("InventoryTracker");
         if(search.Length > 1){
             Destroy(this.gameObject);
         }
         DontDestroyOnLoad(this.gameObject);
+        GameObject temp = GameObject.FindGameObjectWithTag("StatManager");
+        CoffeeStats = temp.GetComponent<FoodStats>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        GameObject temp = GameObject.FindGameObjectWithTag("StatManager");
+        CoffeeStats = temp.GetComponent<FoodStats>();
     }
 
     //adds modifier to amount in inventory linked to key
@@ -117,6 +122,24 @@ public class InventoryTracker : MonoBehaviour
             Debug.LogError("save file not found");
         }
     }
+
+    public void CarryCoffeeStats(){
+        GameObject temp = GameObject.FindGameObjectWithTag("StatManager");
+        CoffeeStats = temp.GetComponent<FoodStats>();
+
+        Debug.Log(CoffeeStats.TextureVal);
+
+        texture = CoffeeStats.TextureVal;
+        flavor = CoffeeStats.FlavorVal;
+        warmth = CoffeeStats.WarmthVal;
+
+        hasFood = true;
+
+        inventory.Clear();
+
+    }
+
+
 }
 
 [System.Serializable]
