29e024f8d8b85715a522ebfe8e14db8ad6c6ab64
Akash D. Kapadia <adkapadi@ucsc.edu>
2021-05-10 11:26:50 -0700

    all weapon damage numbers are public
    check under the gunAnchor on the respective weapon to view and change the numbers in the editor

--- Assets/CombatShit/Scripts/WeaponHandlers/KnifeHandler.cs
+++ Assets/CombatShit/Scripts/WeaponHandlers/KnifeHandler.cs
@@ -8,14 +8,20 @@ public class KnifeHandler : MonoBehaviour
     //public Transform attackPoint;
     //public float attackRange = 0.25f;
     //public LayerMask enemyLayer;
-    public static int knifeDamage = 8;
+    public int knifeDamage = 8;
+    static public int knfDamage;
+
+    private void Start()
+    {
+        knfDamage = knifeDamage; 
+    }
 
     public static void Swing(Vector3 attackPoint, float attackRange, LayerMask enemyLayer)
     {
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint, 1.5f, enemyLayer);
         foreach(Collider2D enemy in hitEnemies)
         {
-            enemy.GetComponent<EnemyBH>().TakeDamage(knifeDamage, Slice);
+            enemy.GetComponent<EnemyBH>().TakeDamage(knfDamage, Slice);
             CameraShake.instance.ShakeCamera(.25f, .03f);
         }
     }
