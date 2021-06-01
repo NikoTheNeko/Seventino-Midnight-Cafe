cd167288853f5b64ec57bf14fa056db534f47a09
Lucio Espinoza <lufespin@ucsc.edu>
2021-06-01 12:02:26 -0700

    Miko poster and inventory background moved in

--- Assets/UI/UI Scripts/OutroShell.cs
+++ Assets/UI/UI Scripts/OutroShell.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class OutroShell : MonoBehaviour
 {
     public TextBoxScript textBox;
-    public TextAsset dialogue;
+    public TextAsset introText;
+    public TextAsset outroText;
+    private Dialogue dialogue;
+    private InventoryTracker tracker;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tracker = GameObject.FindGameObjectWithTag("InventoryTracker").GetComponent<InventoryTracker>();
+        if(tracker.dialogueProg > 0){
+            dialogue = JsonUtility.FromJson<Dialogue>(outroText.text);
+        }
+        else{
+            dialogue = JsonUtility.FromJson<Dialogue>(introText.text);
+        }
     }
 
     // Update is called once per frame
@@ -17,7 +26,7 @@ public class OutroShell : MonoBehaviour
     {
         if(Input.GetButtonDown("Use")){
             if(!textBox.activated){
-                textBox.SetDialogue(JsonUtility.FromJson<Dialogue>(dialogue.text).dialogueSegments);
+                textBox.SetDialogue(dialogue.dialogueSegments);
             }
             else{
                 textBox.SpeedUp();
